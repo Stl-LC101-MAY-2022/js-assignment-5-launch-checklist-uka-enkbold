c3485fe52925d047f57c56e149246fdab67a851a
uka-enkbold <uuganchimeg1104@gmail.com>
2022-08-04 16:56:42 -0500

    4th commitz' ==

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const { myFetch } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
 
-   let listedPlanets = myFetch;
+   let listedPlanets ;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
 
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
@@ -15,4 +15,14 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })  
+   let list = document.getElementById('faultyItems');
+   let form = document.querySelector('form');
+   let pilot  = document.querySelector('input [name = pilotName]');
+   let copilot  = document.querySelector('input [name = copilotName]');
+   let fuelLevel  = document.querySelector('input [name = fuelLevel]');
+   let cargoLevel  = document.querySelector('input [name = cargoMass]');
+   form.addEventListener('submit', function(event){
+    event.preventDefault();
+    formSubmission(document, list, pilotName, copilotName, fuelLevel, cargoMass);
+   }); 
 });
